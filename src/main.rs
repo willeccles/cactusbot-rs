b66690c09d58823495f17907b6aaf6d650c3a0e4
Will Eccles <will@eccles.dev>
2021-02-04 23:21:21 -0500

    !pog

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 extern crate discord;
 
-use discord::model::Event;
+use discord::model::{Event, ChannelId, UserId, Message};
 use discord::Discord;
 use std::env;
+
+fn cmdreply(
+    discord: &Discord,
+    channel: ChannelId,
+    msg: &str
+    ) -> discord::Result<Message> {
+
+    discord.send_message(channel, msg, "", false)
+}
 
 fn main() {
     let discord = Discord::from_bot_token(
@@ -15,28 +24,24 @@ fn main() {
     loop {
         match connection.recv_event() {
             Ok(Event::MessageCreate(message)) => {
-                println!("{} says: {}", message.author.name, message.content);
                 match message.content.split_ascii_whitespace().next() {
                     Some("!hello") => {
-                        let _ = discord.send_message(
-                            message.channel_id,
-                            &("hello, ".to_owned() + &message.author.name),
-                            "",
-                            false,
-                            );
+                        let _ = cmdreply(&discord, message.channel_id,
+                                         &("hello, ".to_owned() + &message.author.name));
+                    }
+                    Some("!pog") => {
+                        let _ = cmdreply(&discord, message.channel_id,
+                                         "<a:kannapogging:753429010184142968>");
                     }
                     Some("!kys") => {
-                        let isme = message.author.id == discord::model::UserId(111943010396229632);
-                        let _ = discord.send_message(
-                            message.channel_id,
-                            if !isme {
-                                "You're not the boss of me >:("
-                            } else {
-                                "<a:kms:805813040327032905>"
-                            },
-                            "",
-                            false,
-                            );
+                        let isme = message.author.id == UserId(111943010396229632);
+                        let _ = cmdreply(&discord, message.channel_id,
+                                        if !isme {
+                                            "You're not the boss of me >:("
+                                        } else {
+                                            "<a:kms:805813040327032905>"
+                                        });
+
                         if isme {
                             break;
                         }
