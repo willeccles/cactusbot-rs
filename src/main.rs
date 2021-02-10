51f8bf7b5e0f177d5eccc1c3b9b850ed8842f211
Will Eccles <will@eccles.dev>
2021-02-09 23:09:12 -0500

    new command parsing

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ fn cmdreply(
     discord.send_message(channel, msg, "", false)
 }
 
+fn cmd_hello(author: &str, arg: &str) -> String {
+    format!("hello, {}", if arg.len() > 0 { arg } else { author })
+}
+
 fn main() {
     let discord = Discord::from_bot_token(
         &env::var("DISCORD_TOKEN").expect("Expected token"))
@@ -24,30 +28,35 @@ fn main() {
     loop {
         match connection.recv_event() {
             Ok(Event::MessageCreate(message)) => {
-                match message.content.split_ascii_whitespace().next() {
-                    Some("!hello") => {
-                        let _ = cmdreply(&discord, message.channel_id,
-                                         &("hello, ".to_owned() + &message.author.name));
-                    }
-                    Some("!pog") => {
-                        let _ = cmdreply(&discord, message.channel_id,
-                                         "<a:kannapogging:753429010184142968>");
-                    }
-                    Some("!kys") => {
-                        let isme = message.author.id == UserId(111943010396229632);
-                        let _ = cmdreply(&discord, message.channel_id,
-                                        if !isme {
-                                            "You're not the boss of me >:("
-                                        } else {
-                                            "<a:kms:805813040327032905>"
-                                        });
-
-                        if isme {
-                            break;
+                // epic(?) new command parsing
+                if message.content.starts_with('!') {
+                    let cmdparts: Vec<&str> = message.content.splitn(2, ' ').collect();
+                    match cmdparts.first().unwrap() {
+                        &"!hello" => {
+                            let _ = cmdreply(&discord, message.channel_id,
+                                             &cmd_hello(&message.author.name,
+                                                        cmdparts.get(1).unwrap_or(&"")));
                         }
+                        &_ => {}
                     }
-                    Some(&_) => {}
-                    None => {}
+                }
+
+                // not epic command parsing maybe
+                if message.content.starts_with("!pog") {
+                    let _ = cmdreply(&discord, message.channel_id,
+                                     "<a:kannapogging:753429010184142968>");
+                } else if message.content.starts_with("!kys") {
+                    let isme = message.author.id == UserId(111943010396229632);
+                    let _ = cmdreply(&discord, message.channel_id,
+                                     if !isme {
+                                         "You're not the boss of me >:("
+                                     } else {
+                                         "<a:kms:805813040327032905>"
+                                     });
+
+                    if isme {
+                        break;
+                    }
                 }
             }
             Ok(_) => {}
